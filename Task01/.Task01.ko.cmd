04bf8af390b56fc0c3a4c2e385dcf3ed6d09b520
Parham Alvani <parham.alvani@gmail.com>
2014-11-10 04:56:30 +0330

    Task01 is added.

---
+++ Task01/.Task01.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/parham/Documents/Git/Kernel-Training/Task01/Task01.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-24-generic/scripts/module-common.lds --build-id  -o /home/parham/Documents/Git/Kernel-Training/Task01/Task01.ko /home/parham/Documents/Git/Kernel-Training/Task01/Task01.o /home/parham/Documents/Git/Kernel-Training/Task01/Task01.mod.o
---
+++ Task01/.tmp_versions/Task01.mod
@@ -0,0 +1,2 @@
+/home/parham/Documents/Git/Kernel-Training/Task01/Task01.ko
+/home/parham/Documents/Git/Kernel-Training/Task01/Task01.o
