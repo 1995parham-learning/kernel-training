2375f05d07c7ee70698c70e5f81f970669b788aa
Parham Alvani <parham.alvani@gmail.com>
2014-12-24 20:52:18 +0330

    NetF project is started.

---
+++ NetF/NetF.c
@@ -0,0 +1,65 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : Task01.c
+ *
+ * [] Creation Date : 21-12-2014
+ *
+ * [] Last Modified : Wed 24 Dec 2014 08:51:40 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * NF_IP_PRE_ROUTING = 0
+ * NF_IP_LOCAL_IN = 1
+ * NF_IP_FORWARD = 2
+ * NF_IP_LOCAL_OUT = 3
+ * WE LOVE U Dr.Bakshi !!!
+*/
+
+#include <linux/module.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/netdevice.h>
+#include <linux/kernel.h>
+#include <linux/printk.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Parham Alvani");
+MODULE_DESCRIPTION("This module is netfilter patch");
+
+static struct nf_hook_ops nfho;
+
+unsigned int hook_func(unsigned int hooknum,
+		struct sk_buff **skb,
+		const struct net_device *in,
+		const struct net_device *out,
+		int (*okfn)(struct sk_buff *))
+{
+	pr_info("%s", out->name);
+	return NF_ACCEPT;
+}
+
+static int __init hello_init(void)
+{
+	pr_info("Setting nf_hook_ops parameters\n");
+
+	/* Fill in our hook structure */
+	nfho.hook = hook_func;
+	nfho.hooknum = 3;
+	nfho.pf = PF_INET;
+	nf_register_hook(&nfho);
+
+	pr_info("Our nf_hook_ops registered\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	nf_unregister_hook(&nfho);
+	pr_info("Our nf_hook_ops unregistered\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
