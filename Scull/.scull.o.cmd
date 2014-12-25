7d8c4fcf5b296e5ce9908d477c022f9dc7817e77
Parham Alvani <parham.alvani@gmail.com>
2014-12-25 18:53:32 +0330

    Scull object file removed.

--- Scull/.scull.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/parham/Documents/Git/Kernel-Training/Scull/scull.o := ld -m elf_x86_64   -r -o /home/parham/Documents/Git/Kernel-Training/Scull/scull.o /home/parham/Documents/Git/Kernel-Training/Scull/main.o 
--- Scull/.tmp_versions/scull.mod
+++
@@ -1,2 +0,0 @@
-/home/parham/Documents/Git/Kernel-Training/Scull/scull.ko
-/home/parham/Documents/Git/Kernel-Training/Scull/main.o
