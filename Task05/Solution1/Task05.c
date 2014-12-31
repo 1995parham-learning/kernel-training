48240daaa22fb50b1de4b423283c30a6dc74e7e4
Parham Alvani <parham.alvani@gmail.com>
2014-12-31 06:56:27 +0330

    Folder structuring is done.

---
+++ Task05/Solution1/Task05.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/printk.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Parham Alvani");
+MODULE_DESCRIPTION("This module does nothing .....\n");
+
+static int __init hello_init(void)
+{
+	pr_info("HelloModule: i am loading\n");
+	pr_debug("HelloModule: Hello World\n");
+
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye, i am unloading\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
