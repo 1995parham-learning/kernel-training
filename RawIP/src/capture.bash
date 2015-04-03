39eaccf5928703bd9e37eb516d8d542bf61ead48
Parham Alvani <parham.alvani@gmail.com>
2015-04-04 00:14:58 +0430

    capture script corrected.

--- RawIP/src/capture.bash
+++ RawIP/src/capture.bash
@@ -6,9 +6,9 @@
 #
 # [] Creation Date : 03-04-2015
 #
-# [] Last Modified : Fri 03 Apr 2015 11:58:13 PM IRDT
+# [] Last Modified : Sat 04 Apr 2015 12:14:52 AM IRDT
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 
-dumpcap -i lo -w $(date +"%F_%H:%m:%S").pcap
+tcpdump -i lo -s 65535 -w $(date +"%F_%H:%m:%S").pcap
