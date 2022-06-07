5a15c791f8070bb682f4b05b580e159e57bea64c
Parham Alvani <parham.alvani@gmail.com>
2022-06-08 02:23:58 +0430

    feat: improve get script

--- kernel/get.sh
+++ kernel/get.sh
@@ -3,3 +3,8 @@
 echo "getting the latest version of the linux kernel"
 
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+
+echo "start with a default configuration"
+
+cd linux || return
+make x86_64_defconfig
