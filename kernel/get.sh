22d0be29c473c14695c25eb32904abd130b28152
Parham Alvani <parham.alvani@gmail.com>
2022-06-18 01:18:48 +0430

    feat: update configuration

--- kernel/get.sh
+++ kernel/get.sh
@@ -3,6 +3,7 @@
 echo "getting the latest version of the linux kernel"
 
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+# git clone --progress -j 0 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 
 echo "start with a default configuration"
 
